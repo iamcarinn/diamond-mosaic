3455ef098aac56bc50d6c6f6391a8210d9d1dfa8
Карина Карпенкова <iamcarinn@MacBook-Pro-Karina.local>
2025-05-22 01:06:37 +0300

    Нанесение символов на картинку, но мало символов

--- go.mod
+++ go.mod
@@ -9,4 +9,7 @@ require (
 	github.com/lucasb-eyer/go-colorful v1.2.0
 )
 
-require golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
+require (
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
+)
--- internal/image/processor.go
+++ internal/image/processor.go
@@ -6,10 +6,31 @@ import (
 	"image/color"
 	"image/draw"
 	"io"
+	"io/ioutil"
+	"log"
+
+	// "golang.org/x/image/font"
 
 	"github.com/disintegration/imaging"
+	"github.com/golang/freetype"
 	"github.com/lucasb-eyer/go-colorful"
 )
+
+// Символы для легенды
+var allSymbols = []string{
+	"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+	"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+	"0","1","2","3","4","5","6","7","8","9",
+	"!","@","#","$","%","^","&","*","(",")","-","_","+","=","~","`",
+	"[","]","{","}","<",">","?","/","\\","|",".",",",":",";","'","\"",
+	// "Ж","З","И","Й","Л","П","Ф","Ц","Ч","Ш","Щ","Э","Ю","Я",
+	// "ж","з","и","й","л","п","ф","ц","ч","ш","щ","э","ю","я",
+}
+
+	// // Присваиваем символы только используемым цветам
+	// for i := 0; i < len(usages) && i < len(allSymbols); i++ {
+	// 	usages[i].PaletteColor.Symbol = allSymbols[i]
+	// }
 
 // ColorUsage связывает цвет из палитры с количеством пикселей (алмазов).
 type ColorUsage struct {
@@ -35,10 +56,26 @@ func Process(file io.Reader, palette []db.PaletteColor) (image.Image, []ColorUsa
 
 	// 4. Подбор ближайших цветов
 	matched := MatchToPalette(resized, palette, gridW, gridH)
+	AssignSymbolsToMatched(matched, allSymbols)
 
 	// 5. Построение растрового холста и подсчёт цветов
 	const cellSize = 10
 	mosaic, usages := RenderMosaic(matched, cellSize)
+
+	rgbaImg, ok := mosaic.(*image.RGBA)
+	if !ok {
+		// если вдруг другой тип, то сконвертируй:
+		bounds := mosaic.Bounds()
+		tmp := image.NewRGBA(bounds)
+		draw.Draw(tmp, bounds, mosaic, bounds.Min, draw.Src)
+		rgbaImg = tmp
+	}
+
+	// нанести символы
+	err = DrawSymbolsOnImage(rgbaImg, matched, cellSize, "fonts/DejaVuSans.ttf")
+	if err != nil {
+		log.Printf("ошибка нанесения символов: %v", err)
+	}
 
 	return mosaic, usages, nil
 }
@@ -158,4 +195,68 @@ func median(data []uint8) uint8 {
 		}
 	}
 	return data[n/2]
+}
+
+// Нарисовать символы на мозаике
+func DrawSymbolsOnImage(img *image.RGBA, matched [][]db.PaletteColor, cellSize int, fontPath string) error {
+	fontBytes, err := ioutil.ReadFile(fontPath)
+	if err != nil {
+		return err
+	}
+	f, err := freetype.ParseFont(fontBytes)
+	if err != nil {
+		return err
+	}
+	c := freetype.NewContext()
+	c.SetDPI(96)
+	c.SetFont(f)
+	c.SetFontSize(float64(cellSize) * 0.7) // размер символа чуть меньше клетки
+	c.SetClip(img.Bounds())
+	c.SetDst(img)
+	c.SetSrc(image.Black) // цвет текста: чёрный
+
+	for y := 0; y < len(matched); y++ {
+		for x := 0; x < len(matched[0]); x++ {
+			sym := matched[y][x].Symbol
+			if sym == "" {
+				continue // если не присвоено, пропускаем
+			}
+			// координаты центра клетки
+			pt := freetype.Pt(
+				x*cellSize+cellSize/4,   // x
+				y*cellSize+cellSize*3/4, // y
+			)
+			_, err := c.DrawString(sym, pt)
+			if err != nil {
+				log.Printf("ошибка рисования символа %q: %v", sym, err)
+			}
+			// log.Printf("(%d,%d): %q", x, y, sym)
+
+		}
+	}
+	return nil
+}
+
+func AssignSymbolsToMatched(matched [][]db.PaletteColor, allSymbols []string) {
+    symbolMap := map[string]string{} // DMC -> символ
+    symbolIdx := 0
+    for y := 0; y < len(matched); y++ {
+        for x := 0; x < len(matched[0]); x++ {
+            pc := &matched[y][x]
+            if pc.Symbol != "" {
+                continue
+            }
+            if sym, ok := symbolMap[pc.DMCCode]; ok {
+                pc.Symbol = sym
+            } else {
+                if symbolIdx < len(allSymbols) {
+                    pc.Symbol = allSymbols[symbolIdx]
+                    symbolMap[pc.DMCCode] = allSymbols[symbolIdx]
+                    symbolIdx++
+                } else {
+                    pc.Symbol = "?" // если символы закончились
+                }
+            }
+        }
+    }
 }
